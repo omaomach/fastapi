4faf6ff344bcfe477bb59c5970d481b2f95ceeef
omaomach <aaronjoash44@gmail.com>
2026-04-03 00:24:06 +0300

    worked on combining query and path parameters

--- hello-world/main.py
+++ hello-world/main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI # import the FastAPI class from the fastapi package
 from enum import Enum
 
@@ -9,11 +10,30 @@ def index():
         'message': 'Hello World'
     }
 
+# @app.get('/blog/all')
+# def get_all_blogs():
+#     return {
+#         'message': 'All blogs provided'
+#     }
+
+# Default Values
+# @app.get('/blog/all')
+# def get_all_blogs(page = 1, page_size = 100): # Query parameters - Any function parameters not part of the path
+#     return {
+#         'message': f"All {page_size} blogs on page {page}"
+#     }
+
+# Optional parameters
 @app.get('/blog/all')
-def get_all_blogs():
+def get_all_blogs(page = 1, page_size: Optional[int] = None):
     return {
-        'message': 'All blogs provided'
+        'message': f"All {page_size} blogs on page {page}"
     }
+
+# Query and Path Parameters
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid:bool = True, username: Optional[str] = None):
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 class BlogType(str, Enum):
     short = 'short'
